e02c3b6788937b2a8b6c36bb8562cfdd4ade2172
rihabchaaban <101224804+achref74@users.noreply.github.com>
2024-04-02 06:22:40 +0000

    integration template front office
    -assets

--- src/Form/UserAdditionalInfoType.php
+++ src/Form/UserAdditionalInfoType.php
@@ -20,10 +20,29 @@ class UserAdditionalInfoType extends AbstractType
             'mapped' => false, // Indique à Symfony de ne pas mapper ce champ à une propriété de l'entité
             'required' => false, // Rend le champ facultatif
         ])
-            ->add('genre')
+        ->add('genre', ChoiceType::class, [
+            'label' => 'Genre',
+            'choices' => [
+                'Homme' => 'homme',
+                'Femme' => 'femme',
+            ],
+            'attr' => [
+                'class' => 'form-control p_input',
+            ],
+        ])            
             ->add('specialite')
-            ->add('niveauAcademique')
-            ->add('disponiblite')
+            ->add('niveauAcademique', ChoiceType::class, [
+                'label' => 'Niveau Académique',
+                'choices' => [
+                    '1-6 primaire' => '1-6 primaire',
+                    '7-9 college' => '7-9 college',
+                    '1-4 lycée' => '1-4 lycée',
+                    'superieur' => 'superieur',
+                ],
+                'attr' => [
+                    'class' => 'form-control p_input',
+                ],
+            ])            ->add('disponiblite')
             ->add('cv', FileType::class, [
                 'label' => 'cv',
                 'mapped' => false, // Indique à Symfony de ne pas mapper ce champ à une propriété de l'entité
