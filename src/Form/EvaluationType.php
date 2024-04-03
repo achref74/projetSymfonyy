ea81405516f2b5106c547fb95153e58f8291599b
loujein <101331416+loujein1-12@users.noreply.github.com>
2024-04-03 19:06:46 +0000

    template fixes

--- src/Form/EvaluationType.php
+++ src/Form/EvaluationType.php
@@ -3,9 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Evaluation;
+use App\Entity\Cours;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType; // Import the CollectionType
+use Symfony\Component\Form\Extension\Core\Type\SubmitType; // Import the SubmitType
 
 class EvaluationType extends AbstractType
 {
@@ -14,7 +18,22 @@ class EvaluationType extends AbstractType
         $builder
             ->add('note')
             ->add('nom')
-            ->add('cours')
+            ->add('cours', EntityType::class, [
+                'class' => Cours::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('questions', CollectionType::class, [ // Add the CollectionType for questions
+                'entry_type' => QuestionType::class, // Use the QuestionType form for each item
+                'entry_options' => ['label' => false], // Hide labels for each question
+                'allow_add' => true, // Allow adding new questions
+                'by_reference' => false, // Set to false to force calling addQuestion() on the Evaluation entity
+                'prototype' => true, // Allow adding a prototype for new questions
+                'prototype_name' => '__question_prototype__', // Name of the prototype form
+                'attr' => [
+                    'class' => 'question-collection', // CSS class for the collection
+                ],
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Save Evaluation']) // Add a submit button
         ;
     }
 
